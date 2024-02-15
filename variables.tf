df1eb78c3b69f0d29c6989a2629871c6c675f4ab
Bhavya Prakash <prakash.b@northeastern.edu>
2024-02-15 03:26:21 -0500

    Added for loops

--- variables.tf
+++ variables.tf
@@ -6,14 +6,35 @@ variable "region" {
   description = "Google Cloud region"
 }
 
-variable "vpc_name" {
-  description = "Name of the VPC"
+variable "vpcs" {
+  description = "A list of VPC configurations"
+  type = map(object({
+    auto_create_subnetworks         = bool
+    routing_mode                    = string
+    delete_default_routes_on_create = bool
+    subnets = map(object({
+      cidr   = string
+      region = string
+    }))
+    route_name       = string
+    dest_range       = string
+    next_hop_gateway = string
+
+  }))
 }
 
-variable "webapp_subnet_cidr" {
-  description = "CIDR range for webapp subnet"
-}
-
-variable "db_subnet_cidr" {
-  description = "CIDR range for db subnet"
+locals {
+  # flatten ensures that this local value is a flat list of objects, rather
+  # than a list of lists of objects.
+  network_subnets = flatten([
+    for network_key, network in var.vpcs : [
+      for subnet_key, subnet in network.subnets : {
+        network_key = network_key
+        subnet_key  = subnet_key
+        network     = google_compute_network.vpc_network[network_key].self_link
+        cidr        = subnet.cidr
+        region      = subnet.region
+      }
+    ]
+  ])
 }
