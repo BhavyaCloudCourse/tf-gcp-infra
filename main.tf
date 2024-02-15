e5fb32f3b68c7db9f24acb5bc0398ba0ecfd6225
Bhavya Prakash <prakash.b@northeastern.edu>
2024-02-15 03:26:21 -0500

    Added for loops

--- main.tf
+++ main.tf
@@ -1,31 +1,26 @@
 resource "google_compute_network" "vpc_network" {
-  name                            = var.vpc_name
+  for_each                        = var.vpcs
+  name                            = each.key
   auto_create_subnetworks         = false
   routing_mode                    = "REGIONAL"
   delete_default_routes_on_create = true
 }
 
-resource "google_compute_subnetwork" "webapp_subnet" {
-  name          = "webapp"
-  ip_cidr_range = var.webapp_subnet_cidr
-  region        = var.region
-  network       = google_compute_network.vpc_network.self_link
+resource "google_compute_subnetwork" "subnet" {
+  for_each = {
+    for subnet in local.network_subnets : "${subnet.network_key}.${subnet.subnet_key}" => subnet
+  }
+  name          = each.value.subnet_key
+  ip_cidr_range = each.value.cidr
+  region        = each.value.region
+  network       = each.value.network
 }
-
-resource "google_compute_subnetwork" "db_subnet" {
-  name          = "db"
-  ip_cidr_range = var.db_subnet_cidr
-  region        = var.region
-  network       = google_compute_network.vpc_network.self_link
-}
-
-# Route for webapp subnet (with destination CIDR range 0.0.0.0/0)
 resource "google_compute_route" "webapp_route" {
-  name             = "webapp-route"
-  network          = google_compute_network.vpc_network.self_link
-  dest_range       = "0.0.0.0/0"
-  next_hop_gateway = "default-internet-gateway"
-  priority         = 1000
+  for_each         = var.vpcs
+  name             = each.value.route_name
+  network          = google_compute_network.vpc_network[each.key].self_link
+  dest_range       = each.value.dest_range
+  next_hop_gateway = each.value.next_hop_gateway
   description      = "Route for webapp subnet"
   tags             = ["webapp"]
 }
