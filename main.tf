60d0f3cbdc153a05fc72fc2cb26e1971df6ebbc0
Bhavya Prakash <prakash.b@northeastern.edu>
2024-03-27 04:29:58 -0400

    Added variables

--- main.tf
+++ main.tf
@@ -30,7 +30,6 @@ resource "google_compute_route" "webapp_route" {
   network          = google_compute_network.vpc_network.self_link
   dest_range       = var.webapp_route_dest_range
   next_hop_gateway = var.webapp_route_next_hop_gateway
-  description      = "Route for webapp subnet"
   tags             = var.webapp_route_tag
 }
 
@@ -38,7 +37,7 @@ resource "google_compute_route" "webapp_route" {
 resource "google_compute_firewall" "allow_app" {
   name               = var.firewall_allow_app_name
   network            = google_compute_network.vpc_network.self_link
-  priority           = 1000
+  priority           = var.firewall_allow_app_priority
   direction          = var.firewall_allow_app_direction
   source_ranges      = var.firewall_allow_app_source_ranges
   destination_ranges = var.firewall_allow_app_destination_ranges
@@ -53,7 +52,7 @@ resource "google_compute_firewall" "allow_app" {
 resource "google_compute_firewall" "deny_ssh" {
   name               = var.firewall_deny_ssh_name
   network            = google_compute_network.vpc_network.self_link
-  priority           = 1001
+  priority           = var.firewall_deny_ssh_priority
   direction          = var.firewall_deny_ssh_direction
   source_ranges      = var.firewall_deny_ssh_source_ranges
   destination_ranges = var.firewall_deny_ssh_destination_ranges
@@ -67,33 +66,33 @@ resource "google_compute_firewall" "deny_ssh" {
 # Create custom image packer family
 data "google_compute_image" "custom_image" {
   project     = var.project_id
-  most_recent = true
+  most_recent = var.custom_image_most_recent
   family      = var.custom_image_family
 }
 
 #Create service account for ops
-resource "google_service_account" "service_account_ops" {
-  account_id   = var.ops_service_account_id
-  display_name = var.ops_service_account_name
+resource "google_service_account" "service_account_vm" {
+  account_id   = var.vm_service_account_id
+  display_name = var.vm_service_account_name
 }
 
 # Create IAM bindings for Logging admin role
 resource "google_project_iam_binding" "logging_admin" {
   project = var.project_id
-  role    = var.ops_service_account_logging_admin_role
+  role    = var.vm_service_account_logging_admin_role
 
   members = [
-    "serviceAccount:${google_service_account.service_account_ops.email}"
+    "serviceAccount:${google_service_account.service_account_vm.email}"
   ]
 }
 
 # Create IAM bindings for Monitoring metrics writer role
 resource "google_project_iam_binding" "metrics_writer" {
   project = var.project_id
-  role    = var.ops_service_account_metrics_writer_role
+  role    = var.vm_service_account_metrics_writer_role
 
   members = [
-    "serviceAccount:${google_service_account.service_account_ops.email}"
+    "serviceAccount:${google_service_account.service_account_vm.email}"
   ]
 }
 
@@ -130,6 +129,8 @@ DB_PASSWORD="${google_sql_user.my_database_sql_user.password}"
 DB_NAME="${google_sql_database.my_database_sql.name}"
 PORT="${var.env_app_port}"
 DB_PORT="${var.env_mysql_port}"
+PROJECT_ID= "${var.project_id}"
+TOPIC_NAME="${google_pubsub_topic.verify_email.name}"
 EOF
 
 
@@ -140,11 +141,11 @@ EOT
   }
 
   service_account {
-    email  = google_service_account.service_account_ops.email
-    scopes = var.ops_service_account_instance_scopes
+    email  = google_service_account.service_account_vm.email
+    scopes = var.vm_service_account_instance_scopes
   }
 
-  allow_stopping_for_update = true
+  allow_stopping_for_update = var.instance_tags_allow_stopping_for_update
 }
 
 
@@ -162,7 +163,7 @@ resource "google_compute_global_address" "private_ip_address" {
 # Create a private connection 
 resource "google_service_networking_connection" "connection" {
   network                 = google_compute_network.vpc_network.self_link
-  service                 = "servicenetworking.googleapis.com"
+  service                 = var.connection_vpc_peering_service
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
 
@@ -179,11 +180,11 @@ resource "google_sql_database_instance" "my_database_instance" {
     ip_configuration {
       ipv4_enabled                                  = var.my_database_instance_ipv4_enabled
       private_network                               = google_compute_network.vpc_network.self_link
-      enable_private_path_for_google_cloud_services = true
+      enable_private_path_for_google_cloud_services = var.my_database_instance_enable_private_path_for_google_cloud_services
     }
     backup_configuration {
-      enabled            = true
-      binary_log_enabled = true
+      enabled            = var.my_database_instance_backup_enabled
+      binary_log_enabled = var.my_database_instance_binary_log_enabled
     }
   }
   deletion_protection = var.my_database_instance_deletion_protection
@@ -198,9 +199,9 @@ resource "google_sql_database" "my_database_sql" {
 
 # Generate a random password for the database user
 resource "random_password" "db_password" {
-  length           = 16
-  special          = true
-  override_special = "!#$%&*()-_=+[]{}<>:?"
+  length           = var.my_database_sql_password_length
+  special          = var.my_database_sql_password_special
+  override_special = var.my_database_sql_password_override
 }
 
 # Create CloudSQL database user
@@ -221,82 +222,80 @@ resource "google_dns_record_set" "Arecord" {
   rrdatas = [google_compute_instance.instance.network_interface[0].access_config[0].nat_ip]
 }
 
+#Create a pubsub topic
 resource "google_pubsub_topic" "verify_email" {
-  name                       = "verify_email"
-  message_retention_duration = "604800s"
+  name                       = var.google_pubsub_topic_name
+  message_retention_duration = var.google_pubsub_topic_message_retention_duration
 }
 
+#Create a pubsub subscription
 resource "google_pubsub_subscription" "verify_email_subs" {
-  name  = "verify_email_subs"
+  name  = var.google_pubsub_subscription_name
   topic = google_pubsub_topic.verify_email.name
 
 }
-resource "google_service_account" "service_account_pubsub" {
-  account_id   = "service-account-pubsub"
-  display_name = "Service Account Pubsub Webapp"
-}
 
-resource "google_project_iam_binding" "publish_binding" {
-  project = var.project_id
-  role    = "roles/pubsub.publisher"
+#Create pubsub topic IAM binding
+resource "google_pubsub_topic_iam_binding" "pubsub_binding" {
+  project = google_pubsub_topic.verify_email.project
+  topic   = google_pubsub_topic.verify_email.name
+  role    = var.pubsub_binding_role
   members = [
-    "serviceAccount:${google_service_account.service_account_pubsub.email}"
+    "serviceAccount:${google_service_account.service_account_vm.email}",
   ]
 }
 
-resource "random_id" "bucket_prefix" {
-  byte_length = 8
-}
-
-
+#Create a service account for cloud function
 resource "google_service_account" "service_account_cloud_function" {
-  account_id   = "service-account-cf-id"
-  display_name = "Service Account Cloud Function"
+  account_id   = var.service_account_cloud_function_id
+  display_name = var.service_account_cloud_function_name
 }
 
-resource "google_project_iam_binding" "cloudsql_binding" {
-  project = var.project_id
-  role    = "roles/cloudsql.client"
+#Create a IAM binding for cloud function service account
+resource "google_cloud_run_v2_service_iam_binding" "cloud_function_binding" {
+  project  = google_cloudfunctions2_function.cloud_function_verify_email.project
+  location = google_cloudfunctions2_function.cloud_function_verify_email.location
+  name     = google_cloudfunctions2_function.cloud_function_verify_email.name
+  role     = var.cloud_function_binding_role
   members = [
-    "serviceAccount:${google_service_account.service_account_cloud_function.email}"
+    "serviceAccount:${google_service_account.service_account_cloud_function.email}",
   ]
 }
-resource "google_project_iam_binding" "networkUser_binding" {
-  project = var.project_id
-  role    = "roles/compute.networkUser"
-  members = [
-    "serviceAccount:${google_service_account.service_account_cloud_function.email}"
-  ]
+resource "random_id" "bucket_prefix" {
+  byte_length = var.bucket_prefix
 }
 
+#Create a bucket to store cloud function code
 resource "google_storage_bucket" "cloud_function_bucket" {
   name                        = "${random_id.bucket_prefix.hex}-gcf-bucket" # Every bucket name must be globally unique
-  location                    = "US"
-  uniform_bucket_level_access = true
+  location                    = var.cloud_function_bucket_location
+  uniform_bucket_level_access = var.cloud_function_bucket_uniform_bucket_level_access
 }
 
+#Zipping Cloud function code
 data "archive_file" "cloud_function_file" {
-  type        = "zip"
-  output_path = "/tmp/serverless.zip"
-  source_dir  = "serverless"
+  type        = var.archive_file_type
+  output_path = var.archive_file_output_path
+  source_dir  = var.archive_file_source_dir
 }
 
+#Creating object in bucket to store serverless zip
 resource "google_storage_bucket_object" "cloud_function_bucket_object" {
-  name   = "serverless.zip"
+  name   = var.cloud_function_bucket_object_name
   bucket = google_storage_bucket.cloud_function_bucket.name
   source = data.archive_file.cloud_function_file.output_path # Path to the zipped function source code
 }
 
+#Creating cloud function
 resource "google_cloudfunctions2_function" "cloud_function_verify_email" {
-  name        = "function2"
-  location    = "us-east4"
-  description = "a new function"
+  name        = var.cloud_function_verify_email_name
+  location    = var.cloud_function_verify_email_location
+  description = var.cloud_function_verify_email_description
 
   build_config {
-    runtime     = "nodejs20"
-    entry_point = "helloPubSub" # Set the entry point
+    runtime     = var.cloud_function_verify_email_build_runtime
+    entry_point = var.cloud_function_verify_email_build_enrtypoint
     environment_variables = {
-      BUILD_CONFIG_TEST = "build_test"
 
     }
     source {
@@ -308,32 +307,38 @@ resource "google_cloudfunctions2_function" "cloud_function_verify_email" {
   }
 
   service_config {
-    max_instance_count = 3
-    min_instance_count = 1
-    available_memory   = "256M"
-    timeout_seconds    = 60
+    max_instance_count = var.cloud_function_verify_email_service_max_instance_count
+    min_instance_count = var.cloud_function_verify_email_service_min_instance_count
+    available_memory   = var.cloud_function_verify_email_service_available_memory
+    timeout_seconds    = var.cloud_function_verify_email_service_timeout_seconds
     environment_variables = {
-      SERVICE_CONFIG_TEST = "config_test"
+      DB_HOST     = "${google_sql_database_instance.my_database_instance.private_ip_address}"
+      DB_USER     = "${google_sql_user.my_database_sql_user.name}"
+      DB_PASSWORD = "${google_sql_user.my_database_sql_user.password}"
+      DB_NAME     = "${google_sql_database.my_database_sql.name}"
+      DB_PORT     = "${var.env_mysql_port}"
 
     }
-    ingress_settings               = "ALLOW_INTERNAL_ONLY"
+    ingress_settings               = var.cloud_function_verify_email_service_ingress_settings
     vpc_connector                  = google_vpc_access_connector.cfconnector.self_link
-    all_traffic_on_latest_revision = true
+    all_traffic_on_latest_revision = var.cloud_function_verify_email_service_all_traffic_on_latest_revision
     service_account_email          = google_service_account.service_account_cloud_function.email
   }
 
   event_trigger {
-    trigger_region = "us-east4"
-    event_type     = "google.cloud.pubsub.topic.v1.messagePublished"
-    pubsub_topic   = google_pubsub_topic.verify_email.id
-    retry_policy   = "RETRY_POLICY_RETRY"
+    trigger_region        = var.cloud_function_verify_email_trigger_region
+    event_type            = var.cloud_function_verify_email_event_type
+    pubsub_topic          = google_pubsub_topic.verify_email.id
+    retry_policy          = var.cloud_function_verify_email_retry_policy
+    service_account_email = google_service_account.service_account_cloud_function.email
   }
 
 }
 
+//Created Serverless VPC Access
 resource "google_vpc_access_connector" "cfconnector" {
-  name          = "vpc-con-serverless"
-  region        = "us-east4"
-  ip_cidr_range = "10.8.0.0/28"
+  name          = var.google_vpc_access_connector_name
+  region        = var.google_vpc_access_connector_region
+  ip_cidr_range = var.google_vpc_access_connector_ip_cidr_range
   network       = google_compute_network.vpc_network.self_link
 }
