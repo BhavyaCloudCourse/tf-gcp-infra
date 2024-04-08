839c4bf96a735c4c08f2bb09c6b5ec8e3d36226d
Bhavya Prakash <prakash.b@northeastern.edu>
2024-04-08 18:07:07 -0400

    Added keys

--- provider.tf
+++ provider.tf
@@ -2,3 +2,8 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+provider "google-beta" {
+  region  = var.region
+  zone    = var.instance_zone
+  project = var.project_id
+}
